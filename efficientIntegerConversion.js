e93ea34e8556b5752ea89367d983a189ea367433
Daniel Dushev <danieldyshew@gmail.com>
2024-04-17 11:01:13 +0300

    efficientIntegerConversion solved

---
+++ efficientIntegerConversion.js
@@ -0,0 +1,14 @@
+function efficientIntegerConversion(num) {
+  let steps = 0;
+  while (num !== 1) {
+    if (num % 2 === 0) {
+      num /= 2;
+    } else if (num === 3 || num % 4 === 1) {
+      num -= 1;
+    } else {
+      num += 1;
+    }
+    steps++;
+  }
+  return steps;
+}
